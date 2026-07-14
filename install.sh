17237852500aba4e765b31deace508b94e0730fd
hooman <hoomankhodadadi91@gmail.com>
2026-07-14 08:19:33 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,98 +2,79 @@
 
 set -e
 
-echo "================================"
-echo " Installing Hooman XRay Panel "
-echo "================================"
+echo "=============================="
+echo " Installing Xray Panel"
+echo "=============================="
 
+
+APP_DIR="/opt/xray-panel"
+
+
+
+echo "[1/8] Updating system..."
 
 apt update -y
+apt upgrade -y
 
 
-echo "[1/8] Installing packages..."
+
+echo "[2/8] Installing packages..."
 
 apt install -y \
-git \
 python3 \
 python3-pip \
 python3-venv \
-mysql-server \
-nginx \
+git \
 curl \
-nodejs \
-npm
+nginx
 
 
 
-echo "[2/8] Setting MySQL..."
+echo "[3/8] Installing NodeJS..."
 
 
-systemctl start mysql
-systemctl enable mysql
+curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
 
-
-DB_NAME="xray_panel"
-DB_USER="panel"
-DB_PASSWORD="panel_password"
-
-
-mysql <<EOF
-
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-
-CREATE USER IF NOT EXISTS '$DB_USER'@'localhost'
-IDENTIFIED BY '$DB_PASSWORD';
-
-GRANT ALL PRIVILEGES ON $DB_NAME.*
-TO '$DB_USER'@'localhost';
-
-FLUSH PRIVILEGES;
-
-
-USE $DB_NAME;
-
-
-CREATE TABLE IF NOT EXISTS users (
-
-id INT AUTO_INCREMENT PRIMARY KEY,
-
-username VARCHAR(100) NOT NULL,
-
-uuid VARCHAR(100) NOT NULL,
-
-vless_link TEXT NOT NULL,
-
-server VARCHAR(255),
-
-port INT
-
-);
-
-EOF
+apt install -y nodejs
 
 
 
-echo "[3/8] Download project..."
+echo "Node:"
+node -v
+
+echo "Python:"
+python3 --version
 
 
-cd /opt
 
 
-if [ ! -d "xray-v2ray-panel" ]; then
+echo "[4/8] Download project..."
 
-git clone https://github.com/hoomanyyy/xray-v2ray-panel.git
+
+
+if [ -d "$APP_DIR" ]
+
+then
+
+cd $APP_DIR
+
+git pull
+
+
+else
+
+git clone https://github.com/hoomanyyy/xray-v2ray-panel.git $APP_DIR
 
 fi
 
 
-cd xray-v2ray-panel
+
+echo "[5/8] Backend setup..."
 
 
 
-echo "[4/8] Backend setup..."
+cd $APP_DIR/backend
 
-
-cd backend
 
 
 python3 -m venv venv
@@ -102,74 +83,39 @@ python3 -m venv venv
 source venv/bin/activate
 
 
+
 pip install --upgrade pip
+
 
 pip install -r requirements.txt
 
-
-cat > .env <<EOF
-
-DB_HOST=localhost
-DB_USER=$DB_USER
-DB_PASSWORD=$DB_PASSWORD
-DB_NAME=$DB_NAME
-
-
-PUBLIC_KEY=YOUR_PUBLIC_KEY
-SHORT_ID=YOUR_SHORT_ID
-SNI=YOUR_SNI
-
-EOF
 
 
 deactivate
 
 
-cd ..
 
 
+echo "[6/8] Creating Backend Service..."
 
-echo "[5/8] Frontend setup..."
-
-
-cd frontend
-
-
-npm install
-
-npm run build
-
-
-rm -rf /var/www/html/*
-
-cp -r build/* /var/www/html/
-
-
-cd ..
-
-
-
-echo "[6/8] Creating backend service..."
 
 
 cat > /etc/systemd/system/xray-panel.service <<EOF
 
 [Unit]
-
-Description=Hooman XRay Panel Backend
-
+Description=Xray Panel Backend
 After=network.target
-
 
 
 [Service]
 
-WorkingDirectory=/opt/xray-v2ray-panel/backend
+WorkingDirectory=$APP_DIR/backend
 
-ExecStart=/opt/xray-v2ray-panel/backend/venv/bin/python app.py
+ExecStart=$APP_DIR/backend/venv/bin/python3 app.py
 
 Restart=always
 
+User=root
 
 
 [Install]
@@ -180,7 +126,9 @@ EOF
 
 
 
+
 systemctl daemon-reload
+
 
 systemctl enable xray-panel
 
@@ -188,7 +136,27 @@ systemctl restart xray-panel
 
 
 
-echo "[7/8] Config nginx..."
+
+
+
+echo "[7/8] Frontend build..."
+
+
+
+cd $APP_DIR/frontend
+
+
+npm install
+
+
+npm run build
+
+
+
+
+
+echo "[8/8] Nginx setup..."
+
 
 
 cat > /etc/nginx/sites-available/xray-panel <<EOF
@@ -204,8 +172,7 @@ server_name _;
 
 
 
-root /var/www/html;
-
+root $APP_DIR/frontend/dist;
 
 index index.html;
 
@@ -227,6 +194,9 @@ location /api/ {
 proxy_pass http://127.0.0.1:8000;
 
 
+proxy_http_version 1.1;
+
+
 proxy_set_header Host \$host;
 
 
@@ -236,7 +206,6 @@ proxy_set_header X-Real-IP \$remote_addr;
 }
 
 
-
 }
 
 
@@ -244,11 +213,13 @@ EOF
 
 
 
-rm -f /etc/nginx/sites-enabled/default
 
-
-ln -s /etc/nginx/sites-available/xray-panel \
+ln -sf /etc/nginx/sites-available/xray-panel \
 /etc/nginx/sites-enabled/xray-panel
+
+
+
+rm -f /etc/nginx/sites-enabled/default
 
 
 
@@ -259,15 +230,18 @@ systemctl restart nginx
 
 
 
-echo "[8/8] Finished"
-
-
 echo ""
-echo "================================"
-echo " Panel Installed Successfully "
-echo "================================"
+echo "=============================="
+echo " Installation Finished"
+echo "=============================="
 
 echo ""
 
-echo "Open:"
-echo "http://SERVER_IP"
+echo "Panel:"
+echo "http://YOUR_SERVER_IP"
+
+
+echo ""
+
+echo "Backend:"
+echo "systemctl status xray-panel"
